5add2e70a64a44fdaecb49c9ebb76d065820b83c
bugtype <bugtype.kr@gmail.com>
2021-05-30 14:23:46 +0900

    feat: added the template file of temp

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,20 @@ export function activate(context: vscode.ExtensionContext) {
     createReactComponentAction
   );
 
+  // TODO: todo
+  // const disposable2 = vscode.commands.registerCommand(
+  //   'generate-react-component.generate.react.component.design',
+  //   createReactComponentAction
+  // );
+
+  // const disposable3 = vscode.commands.registerCommand(
+  //   'generate-react-component.generate.react.component.class',
+  //   createReactComponentAction
+  // );
+
   context.subscriptions.push(disposable);
+  // context.subscriptions.push(disposable2);
+  // context.subscriptions.push(disposable3);
 }
 
 export function deactivate() {}
