f21ebb8b81a3a3cb3a52069333a1f5e2ff1687da
bugtype <bugtype.kr@gmail.com>
2021-06-06 12:15:19 +0900

    refactor: added props in image tempalte

--- src/templates/html-elements/img.ts
+++ src/templates/html-elements/img.ts
@@ -5,9 +5,12 @@ import React, { HTMLAttributes, ImgHTMLAttributes } from 'react';
 interface ComponentNameProps extends ImgHTMLAttributes<HTMLImageElement> {
   //
   alt: string; // a11y required
+  'data-src'?: string; // loading
 }
 
 export const ComponentName = (props: ComponentNameProps) => {
-  return <img {...props} />;
+  const { alt, ...other } = props;
+
+  return <img alt={alt} {...other} />;
 };
 `;
