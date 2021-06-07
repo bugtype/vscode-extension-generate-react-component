5b72f8e790fb24981f5c7bfa410b4d1cc4315c47
bugtype <bugtype.kr@gmail.com>
2021-06-07 21:12:23 +0900

    refactor: clean up the core code.

--- src/core/create-react-component.ts
+++ src/core/create-react-component.ts
@@ -1,25 +1,28 @@
-import * as vscode from "vscode";
-import * as fs from "fs-extra";
-import path from "path";
-import { USED_REACT_ELEMENTS } from "./const";
+import * as vscode from 'vscode';
+import * as fs from 'fs-extra';
+import path from 'path';
+import { AVAILABLE_REACT_ELEMENTS } from './const';
 
-type UsedElement = keyof typeof USED_REACT_ELEMENTS;
+type AvailableElement = keyof typeof AVAILABLE_REACT_ELEMENTS;
 
-const htmlTemplates = USED_REACT_ELEMENTS;
+const htmlTemplates = AVAILABLE_REACT_ELEMENTS;
+const DEFAULT_COMPONENTS_FOLDER_PATH = 'src/components';
 
-const DEFAULT_COMPONENTS_FOLDER_PATH = "src/components";
+// user component path
 let userComponentFolderPath: string | undefined;
 
+// TODO: adding error handling
 export async function createReactComponentAction() {
   const { componentNameInput, htmlElementInput } = await getUserInputs();
 
+  // TODO: move to getUserInputs;
   // if Not input
   if (
     !componentNameInput ||
     !htmlElementInput ||
     !htmlTemplates[htmlElementInput]
   ) {
-    vscode.window.showErrorMessage("Please check the input");
+    vscode.window.showErrorMessage('Please check the input');
     return;
   }
 
@@ -43,7 +46,7 @@ export async function createReactComponentAction() {
     fs.existsSync(componentFilePath.fsPath) ||
     fs.existsSync(fileIndexPath.fsPath)
   ) {
-    vscode.window.showErrorMessage("The Component already exists.");
+    vscode.window.showErrorMessage('The Component already exists.');
     return;
   }
 
@@ -51,31 +54,60 @@ export async function createReactComponentAction() {
   wsEdit.createFile(fileIndexPath, { ignoreIfExists: true });
   await vscode.workspace.applyEdit(wsEdit);
 
-  /**
-   * file write / index.ts
-   */
+  createIndexFile(fileIndexPath, componentNameInput);
+  creatComponentFile(componentFilePath, componentNameInput, htmlElementInput);
+}
 
+async function getUserInputs() {
+  if (!userComponentFolderPath) {
+    userComponentFolderPath = await vscode.window.showInputBox({
+      title: 'Please Input the component folder path',
+      placeHolder: DEFAULT_COMPONENTS_FOLDER_PATH,
+    });
+
+    if (!userComponentFolderPath) {
+      userComponentFolderPath = DEFAULT_COMPONENTS_FOLDER_PATH;
+    }
+  }
+
+  const componentNameInput = await vscode.window.showInputBox({
+    title: 'Please Input the component name',
+    placeHolder: 'UserNameInput',
+  });
+
+  const htmlElementInput = await vscode.window.showInputBox({
+    title: 'Please HTML Element name',
+    placeHolder: Object.keys(AVAILABLE_REACT_ELEMENTS).join(', '),
+  });
+
+  return {
+    componentNameInput,
+    htmlElementInput: htmlElementInput as AvailableElement | undefined,
+  };
+}
+
+function createIndexFile(
+  fileIndexPath: vscode.Uri,
+  componentNameInput: string
+) {
   fs.writeFile(
     fileIndexPath.fsPath,
     `export * from './${componentNameInput}'`,
-    {
-      encoding: "utf8",
-    },
+    { encoding: 'utf8' },
     handleWriteFile
   );
+}
 
-  /**
-   * file write / component file.tsx
-   */
-
+function creatComponentFile(
+  componentFilePath: vscode.Uri,
+  componentNameInput: string,
+  htmlElementInput: AvailableElement
+) {
   const templatePath = path.resolve(
     __dirname,
-    `templates/html-elements/${htmlTemplates[htmlElementInput].replaceName}.tsx`
+    `templates/${htmlTemplates[htmlElementInput].replaceName}.tsx`
   );
-
-  vscode.window.showInformationMessage(templatePath);
-
-  const buffer = fs.readFileSync(templatePath, "UTF-8");
+  const buffer = fs.readFileSync(templatePath, 'UTF-8');
 
   const changedText = buffer
     .toString()
@@ -87,43 +119,15 @@ export async function createReactComponentAction() {
   fs.writeFile(
     componentFilePath.fsPath,
     changedText,
-    { encoding: "utf8" },
+    { encoding: 'utf8' },
     handleWriteFile
   );
-}
-
-async function getUserInputs() {
-  if (!userComponentFolderPath) {
-    userComponentFolderPath = await vscode.window.showInputBox({
-      title: "Please Input the component folder path",
-      placeHolder: DEFAULT_COMPONENTS_FOLDER_PATH,
-    });
-
-    if (!userComponentFolderPath) {
-      userComponentFolderPath = DEFAULT_COMPONENTS_FOLDER_PATH;
-    }
-  }
-
-  const componentNameInput = await vscode.window.showInputBox({
-    title: "Please Input the component name",
-    placeHolder: "UserNameInput",
-  });
-
-  const htmlElementInput = await vscode.window.showInputBox({
-    title: "Please HTML Element name",
-    placeHolder: Object.keys(USED_REACT_ELEMENTS).join(", "),
-  });
-
-  return {
-    componentNameInput,
-    htmlElementInput: htmlElementInput as UsedElement | undefined,
-  };
 }
 
 function handleWriteFile(error: NodeJS.ErrnoException) {
   if (error) {
     vscode.window.showErrorMessage(error.message);
   } else {
-    vscode.window.showInformationMessage("Generated React Component");
+    vscode.window.showInformationMessage('Generated React Component');
   }
 }
